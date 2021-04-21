88c9eef7a65c32a26021502d447a30935db51e32
Клевакин Артем Николаевич <themightymasha@gmail.com>
2021-04-21 19:47:05 +0500

    Layout Works.tsx, work in progress...

--- src/component/Works/Work/Work.tsx
+++ src/component/Works/Work/Work.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import s from "./Work.module.css"
 
-export const Work = () => {
+type WorkType = {
+    title: string
+    description: string
+    link: string
+    img: string
+}
+
+export const Work: React.FC<WorkType> = ({title, description, link, img}) => {
     return (
         <li className={s.work}>
             <div className={s.work_img}>
-                <a>Смотреть</a>
+                <img src={img} alt=""/>
+                <a href={link}>Смотреть</a>
             </div>
-            <h3>Название проекта</h3>
-            <p>Краткое описание</p>
+            <h3><a href={link}>{title}</a></h3>
+            <p>{description}</p>
         </li>
     )
 }
--- src/component/Works/Works.tsx
+++ src/component/Works/Works.tsx
@@ -5,10 +5,42 @@ import s from "./Works.module.css"
 export const Works = () => {
     return (
         <div className={s.works}>
-            <h2>Мои работы</h2>
+            <h2>My works</h2>
             <ul className={s.works_list}>
-                <Work/>
-                <Work/>
+                <Work title={"Social network"}
+                      img={''}
+                      link={"https://feraverto.github.io/network-learning-project-typescript"}
+                      description={"Social networkSocial network Social networkSocial " +
+                      "networkSocial networkSocial networkSocial networkSocial networknetworkSocial networkSocial network"}/>
+
+                <Work title={"Todolist"}
+                      img={''}
+                      link={"http://FeraVerto.github.io/todo"}
+                      description={"Social networkSocial network Social networkSocial " +
+                      "networkSocial networkSocial networkSocial networkSocial network"}/>
+
+                <Work title={"Mini project"}
+                      img={''}
+                      link={'https://feraverto.github.io/simple-react-homeworks/'}
+                      description={"Social networkSocial network Social networkSocial " +
+                      "networkSocial networkSocial networkSocial networkSocial network"}/>
+
+                <Work title={"Cards + Packs"}
+                      img={'../../../image/Packs.png'}
+                      link={"https://FeraVerto.github.io/cards"}
+                      description={"https://FeraVerto.github.io/cards"}/>
+
+                <Work title={"Counter"}
+                      img={''}
+                      link={'https://feraverto.github.io/counter-variant/'}
+                      description={"Social networkSocial network Social networkSocial " +
+                      "networkSocial networkSocial networkSocial networkSocial network"}/>
+
+                <Work title={"Counter"}
+                      img={''}
+                      link={'https://feraverto.github.io/counter-variant/'}
+                      description={"Social networkSocial network Social networkSocial " +
+                      "networkSocial networkSocial networkSocial networkSocial network"}/>
             </ul>
         </div>
     )
