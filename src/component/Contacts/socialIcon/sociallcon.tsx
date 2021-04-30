053b5a88c565b5c8ba4f5cfb6b5b08e618bdfc5f
Клевакин Артем Николаевич <themightymasha@gmail.com>
2021-04-30 20:55:15 +0500

    added links to SocialIcon

--- src/component/Contacts/socialIcon/sociallcon.tsx
+++ src/component/Contacts/socialIcon/sociallcon.tsx
@@ -8,13 +8,19 @@ export const SocialIcon = () => {
     return (
         <ul className={s.social_list}>
             <li className={s.social_item}>
-                <img src={github} alt="github" width={30} height={30}/>
+                <a href="https://github.com/FeraVerto">
+                    <img src={github} alt="github" width={30} height={30}/>
+                </a>
             </li>
             <li className={s.social_item}>
-                <img src={telegram} alt="telegram" width={30} height={30}/>
+                <a href="https://t.me/Mariya_Klevakina">
+                    <img src={telegram} alt="telegram" width={30} height={30}/>
+                </a>
             </li>
             <li className={s.social_item}>
-                <img src={linkedin} alt="linkedin" width={30} height={30}/>
+                <a href="https://www.linkedin.com/in/maria-klevakina/">
+                    <img src={linkedin} alt="linkedin" width={30} height={30}/>
+                </a>
             </li>
         </ul>
     )
